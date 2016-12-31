3d6fa5aa95a6ad2a6ba5caeeeafa2dc888bc0896
agungdp22 <agungdwiprasetyo22@gmail.com>
2016-12-31 14:52:35 +0700

    edit start program

--- start.py
+++ start.py
@@ -6,13 +6,13 @@ isTrain = raw_input("Sudah ada model SVM? (y/n): ")
 if isTrain=="n":
 	# Mengekstraksi ciri, untuk mendapatkan data vektor objek dan bukan objek yang akan dideteksi,
 	# dalam hal ini memisahkan objek dengan background. (Proses ekstraksi menggunakan HOG)
-	print "Step 1: Ekstrak fitur vektor dari data latih citra"
+	print "Ekstrak fitur vektor dari data latih citra.."
 	EkstrakFitur().startExtract()
 
 # tes objek citra yg akan dideteksi ikannya
-print "\nStep 3: Proses mendeteksi objek dari model yang telah dilatih"
+print "\nProses mendeteksi objek dari model yang telah dilatih.."
 
-noCit = raw_input("Citra: ")
+noCit = raw_input("Pilih citra (1 atau 2):")
 tesCitra = "data/datalatih/Ikan/TestImages/"+str(noCit)+".jpg"
 downscale = 2.5 # untuk skala pengecilan citra yg dites, default yg ada di kelas = 2.5x
 visualisasi = True # untuk melihat proses pendeteksian objek pada citra saat sliding windows, default yg di kelas = True
